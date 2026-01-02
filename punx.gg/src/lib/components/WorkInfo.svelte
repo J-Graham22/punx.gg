a54e2cef4f88342b742372e34de0c2c9679fddeb
J-Graham22 <justin.m.graham2@gmail.com>
2026-01-02 01:41:11 -0500

    initial commit

---
+++ punx.gg/src/lib/components/WorkInfo.svelte
@@ -0,0 +1,15 @@
+<script>
+let employer: string;
+let title: string;
+let current: bool;
+let startDate: DateTime;
+let endDate: DateTime;
+let shortDesc: string;
+let longDesc: string;
+</script>
+
+<div>
+</div>
+
+<style>
+</style>
---
+++ punx.gg/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<h1>PUNX [dot] gg</h1>
+
+<h2>about me</h2>
+<p>Justin Graham, a.k.a PUNX</p>
+<p>currently Escalation Engineer @ Varonis</p>
+<p>BS in CS from UNC Chapel Hill</p>
+
+<h2>about me</h2>
+<p>Justin Graham, a.k.a PUNX</p>
+<p>currently Escalation Engineer @ Varonis</p>
+<p>BS in CS from UNC Chapel Hill</p>
+
+<h2>tech stack</h2>
+<p>most comfortable with c#, python, and typescript/javascript</p>
+<p>most excited to use zig, go, and c</p>
+<p>check out the full list of technologies I know</p>
+
+<div class="work">
+</div>
+
+<div class="projects">
+</div>
+
+<p>I'm PUNX, a.k.a. Justin Graham. I'm a software engineer and game dev based in Smithfield, NC.</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
